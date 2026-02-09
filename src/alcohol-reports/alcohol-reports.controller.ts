0947aa48e3137ae9080759a8aaacd7e0a8707c7c
bekhruzdee <qutbidinovbehruz003@gmail.com>
2026-02-09 12:29:55 +0500

    updated

--- src/alcohol-reports/alcohol-reports.controller.ts
+++ src/alcohol-reports/alcohol-reports.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AlcoholReportsService } from './alcohol-reports.service';
 import { CreateAlcoholReportDto } from './dto/create-alcohol-report.dto';
 import { UpdateAlcoholReportDto } from './dto/update-alcohol-report.dto';
