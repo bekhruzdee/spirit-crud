942b5d4eef515cd7c013c1870c05e8303ffc1dad
bekhruzdee <qutbidinovbehruz003@gmail.com>
2026-02-11 14:14:11 +0500

    add front url

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://spirit-report.vercel.app'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
